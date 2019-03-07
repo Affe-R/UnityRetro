bd49af3e2bb6b35c5538e480ab7610ff292da432
Alexander Lövberg <lovbergalexander@gmail.com>
2019-03-07 12:44:13 +0100

    Move and input components
    Jump is still buggy but movement and input works :)

---
+++ RetroUnity/Assets/Scripts/InputSystem/InputComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputComponent : MonoBehaviour
+{
+    public UnityEventVector2 onMove;
+    public UnityEvent onJump;
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float verical = Input.GetAxis("Vertical");
+
+        onMove.Invoke(new Vector2(horizontal, verical));
+
+        if(Input.GetButtonDown("Jump"))
+            onJump.Invoke();
+    }
+}
+
+[System.Serializable]
+public class UnityEventVector2 : UnityEvent<Vector2> {}
---
+++ RetroUnity/Assets/Scripts/MoveSystem/MoveComponent.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MoveComponent : MonoBehaviour
+{
+    [Range(0.01f, 5.0f)]
+    public float moveSpeed = 1;
+
+    [Range(0.01f, 5.0f)]
+    public float jumpForce = 1;
+
+    public LayerMask jumpAreas;
+
+    Rigidbody2D rigidbody;
+
+    public void Move(float horizontal)
+    {
+        Vector2 moveDir = new Vector2(horizontal, 0);
+        Move(moveDir);
+    }
+
+    public void Move(Vector2 moveDir)
+    {
+        if(moveDir.magnitude > 1)
+            moveDir = moveDir.normalized;
+
+        moveDir *= moveSpeed;
+        moveDir.y = rigidbody.velocity.y;
+        rigidbody.velocity = moveDir;
+    }
+
+    public void Jump()
+    {
+        if(IsGrounded())
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    public bool IsGrounded()
+    {
+        SpriteRenderer rend = GetComponent<SpriteRenderer>();
+
+        Vector2 origin = (Vector2)rend.bounds.center - new Vector2(0, rend.bounds.size.y / 2);
+        Debug.Log(origin);
+        Vector2 size = Vector2.one * 0.01f;
+        float angle = 0;
+        Vector2 direction = Vector2.down;
+        float distance = 1.0f;
+        
+        RaycastHit2D castHit = Physics2D.BoxCast(origin, size, angle, direction, distance, jumpAreas);
+        Debug.Log(castHit.collider);
+        return castHit.collider != null;
+    }
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+}
