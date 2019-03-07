90f5ab80d75db731ea92748544c22b2cb8a23dca
Alexander Lövberg <lovbergalexander@gmail.com>
2019-03-07 11:54:46 +0100

    game state machine system
    Not really a state machine but helps with changing scene and time scale currently

---
+++ RetroUnity/Assets/Scripts/GameStateMachineSystem/GameState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "GameState")]
+public class GameState : ScriptableObject
+{
+    public Object scene;
+    public float timeScale = 1;
+    public MonoBehaviour[] behaviors;
+
+    GameStateMachine stateMachine;
+
+    public void Load()
+    {
+        this.stateMachine = GameStateMachine.GetInstance();
+        stateMachine.LoadState(this);
+    }
+}
---
+++ RetroUnity/Assets/Scripts/GameStateMachineSystem/GameStateMachine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateMachine
+{
+    public string mainMenuSceneName;
+    public string playingSceneName;
+
+    static GameStateMachine instance;
+
+    static public GameStateMachine GetInstance()
+    {
+        if(instance == null)
+        {
+            instance = new GameStateMachine();
+        }
+
+        return instance;
+    }
+
+    public void LoadState(GameState state)
+    {
+        if(state.scene != null)
+            SceneManager.LoadScene(state.scene.name);
+            
+        Time.timeScale = state.timeScale;
+    }
+}
