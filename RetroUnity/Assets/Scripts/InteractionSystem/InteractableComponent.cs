d1cb1dd6e1602c9818e8b2b321a916f661c0ed7b
LeafMountain <lovbergalexander@gmail.com>
2019-03-14 11:22:51 +0100

    Interactable system

---
+++ RetroUnity/Assets/Scripts/InteractionSystem/InteractableComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractableComponent : MonoBehaviour
+{
+    public UnityEvent OnInteracted;
+    
+    public void Interact(InteractorComponent interactor)
+    {
+        OnInteracted.Invoke();
+    }
+}
---
+++ RetroUnity/Assets/Scripts/InteractionSystem/InteractorComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractorComponent : MonoBehaviour
+{
+    public float Reach = 1;
+
+    public void Interact()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, Reach);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            colliders[i].GetComponent<InteractableComponent>()?.Interact(this);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, Reach);
+    }
+}
