6e704f236dc29ce718ae53fa7d3d5b6b3aa4f712
MisterDenim <magnus.jakobsson@futuregames.nu>
2019-03-19 11:56:23 +0100

    asdfgh

---
+++ RetroUnity/Assets/Audio/AudioScripts/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource SfxSource;
+    public AudioSource inGameMusicSource;
+    public static AudioManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySingle(AudioClip clip)
+    {
+        SfxSource.clip = clip;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
