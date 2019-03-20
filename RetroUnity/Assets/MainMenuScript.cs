abecd8e6c006dfb93a4e9cc11f9f6b817c817f84
CM\alfred.ruckle <ac.ruckle@gmail.com>
2019-03-20 15:00:20 +0100

    MainMenuUpdate waiting for highscoreList

---
+++ RetroUnity/Assets/MainMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Transform bgSprite;
+    public Vector3 bgStartPos;
+    public Vector3 bgEndPos;
+
+
+    void Update()
+    {
+        if(bgSprite != null)bgSprite.transform.position = Vector3.LerpUnclamped(bgSprite.transform.position, bgEndPos, Time.deltaTime); 
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Playing",LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
