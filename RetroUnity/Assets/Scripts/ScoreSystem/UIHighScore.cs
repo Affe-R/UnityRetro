5f5e7d16a6824736aa0abd6fd71ce3e06eec61db
LeafMountain <lovbergalexander@gmail.com>
2019-03-22 08:21:13 +0100

    Build and working high score

---
+++ RetroUnity/Assets/Scripts/ScoreSystem/UIHighScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHighScore : MonoBehaviour
+{
+    public Text text;
+    void Start()
+    {
+        string holder = ScoreSystem.GetInstance().GetHighscoreHolder();
+        string score = ScoreSystem.GetInstance().GetHighscore().ToString();
+        if(!text)
+            text = GetComponent<Text>();
+
+        if(text)
+            text.text = holder + " " + score;
+    }
+}
